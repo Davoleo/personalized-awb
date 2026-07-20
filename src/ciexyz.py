da6675e842a7cee2d8c2b089fba786e1722625c0
davoleo <dav@davoleo.net>
2026-07-20 16:04:19 +0200

    cie xyz conversion matrix algorithm + scipy lib (used in colour-science)

--- src/ciexyz.py
+++ src/ciexyz.py
@@ -3,10 +3,15 @@ from dataclasses import dataclass
 import json
 from pathlib import Path
 
+from numpy.typing import ArrayLike
 import numpy as np
 import cv2 as cv
+import colour
 
 from src import get_project_dir
+
+STANDARD_A_CCT = 2856
+D65_CCT = 6500
 
 @dataclass
 class Metadata:
@@ -23,10 +28,25 @@ def convert_to_ciexyz(image, filename: str):
     metadata_path = get_project_dir() / "data" / 'Gehler-Shi' / Path(filename.strip(".png") + "_metadata.json")
     meta = extract_metadata(metadata_path)
 
+    _, cct = get_xyz_coords(meta)
     
+    forward_matrix = interpolate_cct_lin(cct, m1=meta.forward_matrix_1, m2=meta.forward_matrix_2)
 
-    print("illu from metadata object: ", meta.illuminant)    
-    pass
+def get_xyz_coords(meta: Metadata):
+    xy: ArrayLike = [0.3127, 0.3290]
+
+    while True:
+        cct = colour.temperature.xy_to_CCT(xy)
+        print(cct)
+        color_matrix = interpolate_cct_lin(cct, meta.color_matrix_1, meta.color_matrix_2)
+        color_matrix_inv = np.linalg.inv(color_matrix)
+        xyz = np.dot(color_matrix_inv, np.transpose(meta.illuminant))
+        X, Y, Z = np.asarray(xyz).flatten()
+        print("X Y Z: ", X, Y, Z)
+        xy_new = [X / (X+Y+Z), Y / (X+Y+Z)]
+        if np.allclose(xy, xy_new, atol=1e-6):
+            return xyz, cct
+        xy = xy_new
 
 def extract_metadata(metapath: Path) -> Metadata:
     with open(metapath, 'r') as file:
@@ -52,7 +72,17 @@ def extract_metadata(metapath: Path) -> Metadata:
 
     return Metadata(illu, cm1, cm2, fm1, fm2)
 
+def interpolate_cct_lin(cct, m1: ArrayLike, m2: ArrayLike):
+    """cct is the interpolator temperature value"""
+    num = (1 / cct) - (1 / D65_CCT)
+    den = (1 / STANDARD_A_CCT) - (1 / D65_CCT)
+    g = num / den   # Ratio of interpolation
+    
+    # Interpolation of matrices
+    CM = g*m1 + (1-g)*m2
+    return CM
+
 if __name__ == '__main__':
-    image_path = get_project_dir() / 'data' / 'Gehler-Shi' / '8D5U5527_sensorname_Canon1D.png'
+    image_path = get_project_dir() / 'data' / 'Gehler-Shi' / '8D5U5534_sensorname_Canon1D.png'
     image = cv.imread(image_path, flags=cv.IMREAD_UNCHANGED)
     converted = convert_to_ciexyz(image, os.path.basename(image_path))
